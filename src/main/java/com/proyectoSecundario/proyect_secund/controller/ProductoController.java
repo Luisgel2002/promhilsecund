5ea2858de4c51672d2c5f16fe1f9cb6f102fc190
luis <luis985849114@outlook.com>
2025-10-27 23:12:47 -0600

    me sale el editar per ocuando se pone guardar se registra mas no se edita

--- src/main/java/com/proyectoSecundario/proyect_secund/controller/ProductoController.java
+++ src/main/java/com/proyectoSecundario/proyect_secund/controller/ProductoController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -57,4 +58,26 @@ public class ProductoController {
 		return "redirect:/ventas/listado";
 	}
 	
+	@GetMapping("edicion/{id}")
+	public String edicion(@PathVariable Integer id,Model model) {
+		model.addAttribute("lstCategoria",categoriaService.getAll());
+		model.addAttribute("lstProveedor",ventasService.getAll());
+		model.addAttribute("producto",productoService.getOne(id));
+		return "ventas/edicion";
+	}
+	
+	@PostMapping("guardar")
+	public String guardar(@ModelAttribute Productos producto, Model model, RedirectAttributes flash) {
+		var response = productoService.update(producto);
+		
+		if (!response.success) {
+			model.addAttribute("alert",Alert.sweetAlertError(response.mensaje));
+			model.addAttribute("lstCategoria",categoriaService.getAll());
+			model.addAttribute("lstProductos",productoService.getAll());
+			return "ventas/nuevo";
+			
+		}
+		flash.addFlashAttribute("toast",Alert.sweetToast(response.mensaje,"success" , 5000));
+		return "redirect:/ventas/listado";
+	}
 }
--- src/main/java/com/proyectoSecundario/proyect_secund/repository/ProductoRepository.java
+++ src/main/java/com/proyectoSecundario/proyect_secund/repository/ProductoRepository.java
@@ -1,9 +1,11 @@
 package com.proyectoSecundario.proyect_secund.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.proyectoSecundario.proyect_secund.model.Productos;
 
 public interface ProductoRepository extends JpaRepository<Productos, Integer>{
-
+	List<Productos>findAll();
 }
